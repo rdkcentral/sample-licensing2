379d193075c0cbdcf6e7092acaef32cd32e0ed05
LE COZ Cedric <12274523+cedriclecoz@users.noreply.github.comm>
2025-07-16 16:28:19 +0100

    Add sum function and prompt user for input in main

--- test.c
+++ test.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+int sum(int a, int b) {
+    return a + b;
+}
 int main(void)
 {
-   printf("Hello World!\n");
+   int x, y;
+   printf("Enter two integers: ");
+   if (scanf("%d %d", &x, &y) != 2) {
+       printf("Invalid input.\n");
+       return 1;
+   }
+   int result = sum(x, y);
+   printf("The sum of %d and %d is %d\n", x, y, result);
    return 0;
 }
