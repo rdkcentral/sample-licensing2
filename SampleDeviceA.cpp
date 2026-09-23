c8f3a2ee415a64f0aed12df9c3f9bdca4b7d6b49
hasinireddy23 <hasinireddy765@gmail.com>
2026-09-23 04:38:07 -0400

    test: add curl snippet to verify detection on new lines only

--- SampleDeviceA.cpp
+++ SampleDeviceA.cpp
@@ -62,7 +62,47 @@ namespace SampleTestC {
     }
 
     int getCustomStatus() {
-    return 100;
-}
+        return 100;
+    }
+
+    // --- Newly added open-source snippet (curl/libcurl) ---
+    char *escapeUrlString(const char *string, int inlength) {
+        size_t alloc = (inlength ? (size_t)inlength : std::strlen(string)) + 1;
+        char *ns;
+        char *testing;
+        size_t newlen = alloc;
+        int strindex = 0;
+        size_t length;
+
+        ns = (char *)std::malloc(alloc);
+        if(!ns)
+            return nullptr;
+
+        length = alloc - 1;
+        while(length--) {
+            unsigned char in = *string++;
+            if((in >= 'a' && in <= 'z') || (in >= 'A' && in <= 'Z') ||
+               (in >= '0' && in <= '9') || in == '-' || in == '.' ||
+               in == '_' || in == '~') {
+                ns[strindex++] = in;
+            }
+            else {
+                newlen += 2;
+                if(newlen > alloc) {
+                    alloc *= 2;
+                    testing = (char *)std::realloc(ns, alloc);
+                    if(!testing) {
+                        std::free(ns);
+                        return nullptr;
+                    }
+                    ns = testing;
+                }
+                std::sprintf(&ns[strindex], "%%%02X", in);
+                strindex += 3;
+            }
+        }
+        ns[strindex] = 0;
+        return ns;
+    }
 
 }
