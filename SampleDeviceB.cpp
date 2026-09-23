bd6563065d072a6bb5c1dbf05f5a0c60b1e3d85d
hasinireddy23 <hasinireddy765@gmail.com>
2026-09-23 14:14:33 -0400

    Add SampleDeviceB and remove SampleDeviceA  and test with license mode default

---
+++ SampleDeviceB.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <memory>
+#include <string>
+#include <cstdlib>
+#include <cstring>
+
+namespace SampleTestB {
+
+    void testAudioModuleB() {
+        // Implement the audio module test for SampleDeviceB here.
+    }
+
+}
