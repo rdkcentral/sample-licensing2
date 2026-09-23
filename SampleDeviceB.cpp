2a4a682a1fa194e14ec2a6bd6b9703f69e771828
hasinireddy23 <hasinireddy765@gmail.com>
2026-09-23 13:11:02 -0400

    add second snippet to SampleDeviceB

--- SampleDeviceB.cpp
+++ SampleDeviceB.cpp
@@ -62,35 +62,52 @@ namespace SampleTestB {
             tapToTalkAudioProvider);
     }
 
-    void simplePrint(const std::string& stringToPrint) {
-        std::lock_guard<std::mutex> lock(m_mutex);
-        const char* stringPtr = stringToPrint.c_str();
-        const char* endPtr = stringPtr + stringToPrint.length();
-
-        while (stringPtr < endPtr) {
-            const char* nextPtr = stringPtr;
-            while (nextPtr < endPtr && *nextPtr != '\n') {
-                nextPtr++;
-            }
-
-            if (nextPtr != endPtr) {
-                nextPtr++;
-            }
-
-            size_t bytesToWrite = nextPtr - stringPtr;
-            while (bytesToWrite > 0) {
-                ssize_t bytesWritten = write(STDOUT_FILENO, stringPtr, bytesToWrite);
-                if (bytesWritten < 0) {
-                    if (errno == EINTR) {
-                        continue;
-                    }
-                    break;
-                }
-                bytesToWrite -= bytesWritten;
-                stringPtr += bytesWritten;
-            }
-            stringPtr = nextPtr;
+    bool testTransportAndGateway() {
+        // Context
+        auto contextManager = manufactory->get<std::shared_ptr<avsCommon::sdkInterfaces::ContextManagerInterface>>();
+        if (!contextManager) {
+            return false;
         }
+
+        auto avsGatewayManagerStorage = avsGatewayManager::storage::AVSGatewayManagerStorage::create(miscStorage);
+        if (!avsGatewayManagerStorage) {
+            return false;
+        }
+        auto avsGatewayManager = avsGatewayManager::AVSGatewayManager::create(
+            std::move(avsGatewayManagerStorage), customerDataManager, config, authDelegate);
+        if (!avsGatewayManager) {
+            return false;
+        }
+
+        auto synchronizeStateSenderFactory = synchronizeStateSender::SynchronizeStateSenderFactory::create(contextManager);
+        if (!synchronizeStateSenderFactory) {
+            return false;
+        }
+
+        std::vector<std::shared_ptr<avsCommon::sdkInterfaces::PostConnectOperationProviderInterface>> providers;
+        providers.push_back(synchronizeStateSenderFactory);
+        providers.push_back(avsGatewayManager);
+        providers.push_back(m_capabilitiesDelegate);
+
+        /*
+         * Create a factory for creating objects that handle tasks that need to be performed right after establishing
+         * a connection to AVS.
+         */
+        auto postConnectSequencerFactory = acl::PostConnectSequencerFactory::create(providers);
+
+        /*
+         * Create a factory to create objects that establish a connection with AVS.
+         */
+        auto transportFactory = std::make_shared<acl::HTTP2TransportFactory>(
+            std::make_shared<avsCommon::utils::libcurlUtils::LibcurlHTTP2ConnectionFactory>(),
+            postConnectSequencerFactory,
+            nullptr,
+            nullptr);
+        if (!transportFactory) {
+            return false;
+        }
+
+        return true;
     }
 
 }
