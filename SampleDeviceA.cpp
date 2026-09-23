bf9ebfe922de033eba53c2d039d37d03b77e2019
hasinireddy23 <hasinireddy765@gmail.com>
2026-09-23 14:01:38 -0400

    Add second snippet in SampleDeviceA and test with license mode default

--- SampleDeviceA.cpp
+++ SampleDeviceA.cpp
@@ -1,15 +1,12 @@
 #include <iostream>
 #include <memory>
 #include <string>
+#include <cstdlib>
+#include <cstring>
 
 namespace SampleTestC {
 
     void testAudioModuleC() {
-
-
-
-
-        
         std::shared_ptr<alexaClientSDK::defaultClient::DefaultClient> client = alexaClientSDK::defaultClient::DefaultClient::create(
             deviceInfo,
             customerDataManager,
@@ -66,11 +63,52 @@ namespace SampleTestC {
             tapToTalkAudioProvider);
     }
 
-    // ONLY ADDING DUMMY CODE (NOT OPEN SOURCE):
-    void proprietaryBusinessLogic() {
-        int internalCounter = 100;
-        internalCounter += 50;
-        std::cout << "Custom internal counter: " << internalCounter << std::endl;
+    bool testTransportAndGateway() {
+        // Context
+        auto contextManager = manufactory->get<std::shared_ptr<avsCommon::sdkInterfaces::ContextManagerInterface>>();
+        if (!contextManager) {
+            return false;
+        }
+
+        auto avsGatewayManagerStorage = avsGatewayManager::storage::AVSGatewayManagerStorage::create(miscStorage);
+        if (!avsGatewayManagerStorage) {
+            return false;
+        }
+        auto avsGatewayManager = avsGatewayManager::AVSGatewayManager::create(
+            std::move(avsGatewayManagerStorage), customerDataManager, config, authDelegate);
+        if (!avsGatewayManager) {
+            return false;
+        }
+
+        auto synchronizeStateSenderFactory = synchronizeStateSender::SynchronizeStateSenderFactory::create(contextManager);
+        if (!synchronizeStateSenderFactory) {
+            return false;
+        }
+
+        std::vector<std::shared_ptr<avsCommon::sdkInterfaces::PostConnectOperationProviderInterface>> providers;
+        providers.push_back(synchronizeStateSenderFactory);
+        providers.push_back(avsGatewayManager);
+        providers.push_back(m_capabilitiesDelegate);
+
+        /*
+         * Create a factory for creating objects that handle tasks that need to be performed right after establishing
+         * a connection to AVS.
+         */
+        auto postConnectSequencerFactory = acl::PostConnectSequencerFactory::create(providers);
+
+        /*
+         * Create a factory to create objects that establish a connection with AVS.
+         */
+        auto transportFactory = std::make_shared<acl::HTTP2TransportFactory>(
+            std::make_shared<avsCommon::utils::libcurlUtils::LibcurlHTTP2ConnectionFactory>(),
+            postConnectSequencerFactory,
+            nullptr,
+            nullptr);
+        if (!transportFactory) {
+            return false;
+        }
+
+        return true;
     }
 
 }
